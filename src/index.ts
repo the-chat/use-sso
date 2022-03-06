4d57e649500e496d9bd710199dda71c1db62ecc2
the-chat <thechatllc@gmail.com>
2022-03-06 17:55:41 +0500

    user: User

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const getSSO = (auth: Auth, SSOHost: string = SSO.DEFAULT_SSO_HOST) => {
 
   // todo?: useUser handle error
   const useSSO = () => {
-    const [user, , { loading, error }] = useUser()
+    const [, user, { loading, error }] = useUser()
     const { replace } = useRouter()
     const { t } = useTranslation("sso")
     const { handleError } = useLogs(() => {})
