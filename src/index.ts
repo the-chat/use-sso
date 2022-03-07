eb407fd0470ea841c35a38e5b21dec023b69a06c
the-chat <thechatllc@gmail.com>
2022-03-07 17:24:21 +0500

    no getSSO

--- src/index.ts
+++ src/index.ts
@@ -8,55 +8,56 @@ import { useUser } from "@the-chat/use-user"
 import { useEffect } from "react"
 import { SSO } from "@the-chat/config"
 
-const getSSO = (auth: Auth, SSOHost: string = SSO.DEFAULT_SSO_HOST) => {
-  const getSSOLink = () =>
-    setQuery(SSOHost, {
-      "return-url": location.toString(),
-    })
+const getSSOLink = (SSOHost: string = SSO.DEFAULT_SSO_HOST) =>
+  setQuery(SSOHost, {
+    "return-url": location.toString(),
+  })
 
-  // todo?: useUser handle error
-  const useSSO = (newUser = true) => {
-    const [, user, { loading, error }] = useUser()
-    const { replace } = useRouter()
-    const { t } = useTranslation("sso")
-    const { handleError } = useLogs(() => {})
+// todo?: useUser handle error
+const useSSO = (
+  auth: Auth,
+  SSOHost: string = SSO.DEFAULT_SSO_HOST,
+  newUser = true
+) => {
+  const [, user, { loading, error }] = useUser()
+  const { replace } = useRouter()
+  const { t } = useTranslation("sso")
+  const { handleError } = useLogs(() => {})
 
-    const signInLocally = () => {
-      if (!user && !loading && !error) {
-        const query = getQuery(location.toString())
+  const signInLocally = () => {
+    if (!user && !loading && !error) {
+      const query = getQuery(location.toString())
 
-        // if user right now signed in globally
-        if (query.token)
-          return signInWithCustomToken(auth, query["token"]).catch(
-            handleError(t("error.sign-in-token"))
-          )
+      // if user right now signed in globally
+      if (query.token)
+        return signInWithCustomToken(auth, query["token"]).catch(
+          handleError(t("error.sign-in-token"))
+        )
 
-        axios(SSOHost + SSO.getSSOTokenUrl)
-          .then(({ data }) => {
-            if (data.code)
-              switch (data.code) {
-                // if user not signed in globally
-                case "not-signed-in":
-                  newUser && replace(getSSOLink())
-                  break
-              }
-            else if (data.token)
-              // if user signed in globally
-              signInWithCustomToken(auth, data.token).catch(
-                handleError(t("error.sign-in-token"))
-              )
-            else handleError(t("error.get-token-code-" + data.code))()
-          })
-          .catch(handleError(t("error.get-token-request")))
-      }
+      axios(SSOHost + SSO.getSSOTokenUrl)
+        .then(({ data }) => {
+          if (data.code)
+            switch (data.code) {
+              // if user not signed in globally
+              case "not-signed-in":
+                newUser && replace(getSSOLink())
+                break
+            }
+          else if (data.token)
+            // if user signed in globally
+            signInWithCustomToken(auth, data.token).catch(
+              handleError(t("error.sign-in-token"))
+            )
+          else handleError(t("error.get-token-code-" + data.code))()
+        })
+        .catch(handleError(t("error.get-token-request")))
     }
-
-    useEffect(() => {
-      signInLocally()
-    }, [user, loading, error])
   }
 
-  return { getSSOLink, useSSO }
+  useEffect(() => {
+    signInLocally()
+  }, [user, loading, error])
 }
 
-export default getSSO
+export { getSSOLink }
+export default useSSO
